9c2e4263de6a310716fa91d0ed8d4fbf32652dbe
Edy Gunawan <gunawanedy40@gmail.com>
2023-10-25 08:41:47 +0700

    Update year

--- src/Process-Data/Get_Odds_Data.py
+++ src/Process-Data/Get_Odds_Data.py
@@ -12,8 +12,10 @@ from tqdm import tqdm
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from Utils.tools import get_date
 
-year = ["2023", "2024"]
-season = ["2023-24"]
+year = ["2022", "2023",# "2024"
+        ]
+season = ["2022-23", #"2023-24"
+        ]
 
 month = [10, 11, 12, 1, 2, 3, 4, 5, 6]
 days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
