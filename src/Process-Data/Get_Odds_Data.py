464150ba7bc3a0904f7fb0221d5df88fd1e8b926
Edy Gunawan <gunawanedy40@gmail.com>
2023-10-25 07:58:03 +0700

    Update import

--- src/Process-Data/Get_Odds_Data.py
+++ src/Process-Data/Get_Odds_Data.py
@@ -10,7 +10,7 @@ from sbrscrape import Scoreboard
 from tqdm import tqdm
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-from src.Utils.tools import get_date
+from Utils.tools import get_date
 
 year = ["2023", "2024"]
 season = ["2023-24"]
